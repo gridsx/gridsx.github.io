1c74a553e5a595ea207204f32d84b75d9ff75f5c
winjeg <winjeg@qq.com>
2023-03-08 14:21:49 +0800

    complete github address

--- src/components/footer/Index.tsx
+++ src/components/footer/Index.tsx
@@ -1,11 +1,10 @@
-import  React, { FC } from 'react';
+import React, { FC } from 'react';
 import './style.css'
 
 
 const Footer: FC = () => {
-
     return <div className='gridsx-footer'>
-        <div>Links : Github</div>
+        <div> <a href='https://github.com/gridsx/tables_plus'>Links : Github</a></div>
     </div>
 }
 
